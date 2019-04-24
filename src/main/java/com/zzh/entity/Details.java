910a041602e4abf06d61e84768ba1b76043040fe
zhang <boisi@qq.com>
2019-04-24 08:32:28 +0800

    myComputer v1.84

--- src/main/java/com/zzh/entity/Details.java
+++ src/main/java/com/zzh/entity/Details.java
@@ -2,11 +2,17 @@ package com.zzh.entity;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import tk.mybatis.mapper.annotation.KeySql;
+
+import javax.persistence.Id;
 
 @Data
 @NoArgsConstructor
 public class Details {
+    @Id
+    @KeySql(useGeneratedKeys = true)
     private int detailsId;
     private int categoryId;
     private String desc;
+    private Product productId;
 }
--- src/main/java/com/zzh/entity/Parameter.java
+++ src/main/java/com/zzh/entity/Parameter.java
@@ -2,10 +2,15 @@ package com.zzh.entity;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import tk.mybatis.mapper.annotation.KeySql;
+
+import javax.persistence.Id;
 
 @Data
 @NoArgsConstructor
 public class Parameter {
+    @Id
+    @KeySql(useGeneratedKeys = true)
     private int parameterId;
     private Details details;
     private String parameterName;
